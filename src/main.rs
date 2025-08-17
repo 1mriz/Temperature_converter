2d198bc66ad0c6cb4732ca194479d73e5c167f0e
Rizawan <mohammadrizawan500@gmail.com>
2025-08-17 21:56:29 +0600

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::io;
+fn main() {
+    println!("Welcome to temperature converter! ");
+    println!("Choose an option from below:");
+    println!("1. Fahrenheit to Celsius");
+    println!("2. Celsius to Fahrenheit");
+    println!("3. Exit");
+    println!("Enter an option: ");
+    let mut input1 = String::new();
+    io::stdin().read_line(&mut input1).expect("Failed to read input");
+
+    let _option = match input1.trim() {
+        "1" => loop {
+            println!("Enter Fahrenheit to convert to Celsius");
+            let mut input2= String::new();
+            io::stdin().read_line(&mut input2).expect("Failed to read input");
+            let _x = 5 / 9;
+            let input2: f64 = match input2.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Input is not a number!");
+                    continue;
+                },
+            };
+            let res = (input2 - 32.0) * 5.0 / 9.0;
+            println!("{input2} Fahrenheit in Celsius is: {}°", res);
+            break;
+        },
+        "2" => loop {
+            println!("Enter Celsius to convert to Fahrenheit");
+            let mut input2 = String::new();
+            io::stdin().read_line(&mut input2).expect("Failed to read input");
+
+            let input2: f64 = match input2.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Input is not a number!");
+                    continue;
+                },
+            };
+            let res = (input2 * 1.8) + 32.0;
+            println!("{input2} Celsius in Fahrenheit is: {res:.2}°");
+            break;
+        }
+        "3" => println!("Bye!"),
+        _ => println!("Invalid option!"),
+    };
+}
