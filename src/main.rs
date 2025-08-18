aef000f8317b290965c7bcfa42c9c1ffc48b4be6
Rizawan <mohammadrizawan500@gmail.com>
2025-08-18 18:23:35 +0600

    some improvements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 fn main() {
     println!("Welcome to temperature converter! ");
+    loop {
     println!("Choose an option from below:");
     println!("1. Fahrenheit to Celsius");
     println!("2. Celsius to Fahrenheit");
@@ -10,7 +11,7 @@ fn main() {
     io::stdin().read_line(&mut input1).expect("Failed to read input");
 
     let _option = match input1.trim() {
-        "1" => loop {
+        "1" =>  {
             println!("Enter Fahrenheit to convert to Celsius");
             let mut input2= String::new();
             io::stdin().read_line(&mut input2).expect("Failed to read input");
@@ -23,10 +24,10 @@ fn main() {
                 },
             };
             let res = (input2 - 32.0) * 5.0 / 9.0;
-            println!("{input2} Fahrenheit in Celsius is: {}°", res);
-            break;
+            println!("{input2} Fahrenheit in Celsius is: {res:.2}°");
+            
         },
-        "2" => loop {
+        "2" =>  {
             println!("Enter Celsius to convert to Fahrenheit");
             let mut input2 = String::new();
             io::stdin().read_line(&mut input2).expect("Failed to read input");
@@ -40,9 +41,12 @@ fn main() {
             };
             let res = (input2 * 1.8) + 32.0;
             println!("{input2} Celsius in Fahrenheit is: {res:.2}°");
-            break;
+            
         }
-        "3" => println!("Bye!"),
+        "3" => { 
+            println!("Bye!"),
+                break;
+    }
         _ => println!("Invalid option!"),
     };
 }
